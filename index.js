2e06c3769ebdd019f28128601dff1781f85b696a
aaryanvekariya <aaryanvekariya0143@gmail.com>
2025-01-08 10:29:46 +0530

    V 0.0.2

--- index.js
+++ index.js
@@ -22,18 +22,18 @@
 import ollama from "ollama";
 import fs from "fs";
 
-let q= fs.readFileSync("./q.txt", "utf-8");
+let q= fs.readFileSync("./A.txt", "utf-8");
 console.log(q)
 
 askQuestion()
 async function askQuestion() {
   try {
     const response = await ollama.chat({
-      model: "llama3.2:1b",
+      model: "llama3.2:latest",
       messages: [{ role: 'user', content: q }]
     });
 
-    fs.writeFileSync("./a.txt", response.message.content);
+    console.log(response.message.content);
 
   } catch (error) {
     console.error("Error occurred:", error.message);
