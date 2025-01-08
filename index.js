914e2bb278595fcb4c929f956cb26aeafb61822b
aaryanvekariya <aaryanvekariya0143@gmail.com>
2025-01-08 09:59:30 +0530

    Assignmnet14- GenAI - 2203031080125

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-import ollama from "ollama";
+// import ollama from "ollama";
 
-async function runChat() {
+// async function runChat() {
+//   try {
+//     const response = await ollama.chat({
+//       model: "llama3.2:latest",
+//       messages: [{ role: 'user', content: "Generate a research abstract" }]
+//     });
+
+//     console.log("Chatbot Response:", response.message.content);
+//   } catch (error) {
+//     console.error("Error occurred:", error.message);
+//   }
+// }
+
+// runChat();
+
+
+// 2ND STAGE
+
+
+import ollama from "ollama";
+import fs from "fs";
+
+let q= fs.readFileSync("./q.txt", "utf-8");
+console.log(q)
+
+askQuestion()
+async function askQuestion() {
   try {
     const response = await ollama.chat({
-      model: "llama3.2:latest",
-      messages: [{ role: 'user', content: "Generate a research abstract" }]
+      model: "llama3.2:1b",
+      messages: [{ role: 'user', content: q }]
     });
 
-    console.log("Chatbot Response:", response.message.content);
+    fs.writeFileSync("./a.txt", response.message.content);
+
   } catch (error) {
     console.error("Error occurred:", error.message);
   }
 }
-
-runChat();
